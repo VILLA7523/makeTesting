5fb8b3011dd0513758f66bbaf127d4a1a9b2b0a1
villas12villas34 <fvillarroel@unsa.edu.pe>
2022-11-14 07:24:12 -0500

    second commit

--- sample1Unittest.cc
+++ sample1Unittest.cc
@@ -38,21 +38,6 @@ TEST(FactorialTest, Negative)
   EXPECT_EQ(1, Factorial(-5));
   EXPECT_EQ(1, Factorial(-1));
   EXPECT_GT(Factorial(-10), 0);
-
-  // <TechnicalDetails>
-  //
-  // EXPECT_EQ(expected, actual) is the same as
-  //
-  //   EXPECT_TRUE((expected) == (actual))
-  //
-  // except that it will print both the expected value and the actual
-  // value when the assertion fails.  This is very helpful for
-  // debugging.  Therefore in this case EXPECT_EQ is preferred.
-  //
-  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
-  // and is thus more general.
-  //
-  // </TechnicalDetails>
 }
 
 // Tests factorial of 0.
